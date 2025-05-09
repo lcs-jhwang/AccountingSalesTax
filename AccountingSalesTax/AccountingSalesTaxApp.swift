cb02251698ef1a0978d6127c2bec7df9aa976b3b
Julien Hwang <jhwang@lcs.on.ca>
2025-05-09 15:11:36 -0400

    Initial Commit

---
+++ AccountingSalesTax/AccountingSalesTaxApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountingSalesTaxApp.swift
+//  AccountingSalesTax
+//
+//  Created by Julien Hwang on 9/5/2025.
+//
+
+import SwiftUI
+
+@main
+struct AccountingSalesTaxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
